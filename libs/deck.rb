ccfed8f182735dd75eba443d51f380c31576bd37
elbub1979 <ilya.leuh09@gmail.com>
2024-10-18 17:28:49 +0300

    add cards suits & ranks in deck

--- libs/deck.rb
+++ libs/deck.rb
@@ -1,7 +1,28 @@
 class Deck
-  CARDS_TYPE = {}.freeze
+  # CARDS_TYPE = {}.freeze
+
+  SUITS = %w[Hearts Diamonds Clubs Spades].freeze
+  RANKS = %w[2 3 4 5 6 7 8 9 10 Jack Queen King Ace].freeze
+
+  class << self
+    def create_deck
+      cards = []
+
+      SUITS.each do |suit|
+        RANKS.each do |rank|
+          cards << { suit: suit, rank: rank }
+        end
+      end
+
+      new(cards)
+    end
+  end
 
   attr_accessor :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
 
   def count_points(cards); end
 
